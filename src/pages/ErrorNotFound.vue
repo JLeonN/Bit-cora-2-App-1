6f8733b222fb37f2bc362201a86b5c5660b208fa
Leonardo Núñez <jorgebike.75@hotmail.com>
2025-08-01 18:47:30 -0300

    Mejorando el error 404

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,23 +1,48 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">404</div>
-
-      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
-
+  <div class="contenedor-error">
+    <div class="contenido">
+      <h1 class="codigo-error">404</h1>
+      <p class="mensaje-error">Oops... No encontramos esa página.</p>
       <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
+        label="Volver al inicio"
         to="/"
-        label="Inicio"
+        color="color-acento"
+        text-color="white"
+        unelevated
         no-caps
+        class="boton-volver"
       />
     </div>
   </div>
 </template>
-
-<script setup>
-//
-</script>
+<!----------- CSS ----------->
+<style scoped>
+.contenedor-error {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--color-fondo);
+  color: var(--color-texto-principal);
+  text-align: center;
+  padding: 20px;
+}
+.contenido {
+  max-width: 500px;
+}
+.codigo-error {
+  font-size: 20vh;
+  margin: 0;
+  color: var(--color-primario);
+}
+.mensaje-error {
+  font-size: 1.7rem;
+  color: var(--color-texto-secundario);
+  margin-bottom: 40px;
+}
+.boton-volver {
+  font-size: 1rem;
+  padding: 10px 30px;
+  background-color: var(--color-acento);
+}
+</style>
