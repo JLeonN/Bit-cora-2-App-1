4a2499fb8bab8897f4379c82ee5f5331aaffa4b5
Leonardo Núñez <jorgebike.75@hotmail.com>
2025-09-15 06:38:36 -0300

    Imagen splash

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,11 @@
 </template>
 
 <script setup>
-//
+import { onMounted } from 'vue'
+import { SplashScreen } from '@capacitor/splash-screen'
+
+onMounted(() => {
+  // Ocultar splash cuando la app esté pronta
+  SplashScreen.hide()
+})
 </script>
